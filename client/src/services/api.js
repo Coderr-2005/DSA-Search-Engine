d119990585f232e6cdfe75e2f2d24c48510e6946
Abhay Chauhan <abhaychauhank0@gmail.com>
2026-07-16 05:17:26 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://algoatlas-backend-8cyw.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
